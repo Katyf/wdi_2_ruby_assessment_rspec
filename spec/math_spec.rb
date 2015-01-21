1faf4af8d5eb21f1cd7e7f6b9ae53ae0a46d7dee
katyf <katy.feng.14@gmail.com>
2015-01-21 09:08:47 -0500

    RSpec Assignment Complete

--- spec/math_spec.rb
+++ spec/math_spec.rb
@@ -13,3 +13,35 @@ RSpec.describe '#whatever method I'm testing' do
 end
 
 =end
+
+RSpec.describe '#square' do
+  it 'squares the given number' do
+    expect(square(2)).to eq 4
+    expect(square(3)).to eq 9
+    expect(square(10)).to eq 100
+  end
+end
+
+RSpec.describe '#cube' do
+  it 'cubes the given number' do
+    expect(cube(2)).to eq 8
+    expect(cube(3)).to eq 27
+    expect(cube(10)).to eq 1000
+  end
+end
+
+RSpec.describe '#perimeter' do
+  it 'gives the perimeter of a square for a sidelength' do
+    expect(perimeter(2)).to eq 8
+    expect(perimeter(3)).to eq 12
+    expect(perimeter(10)).to eq 40
+  end
+end
+
+RSpec.describe '#cube_surface_area' do
+  it 'gives the surface area of a cube for a given edge length' do
+    expect(cube_surface_area(2)).to eq 24
+    expect(cube_surface_area(3)).to eq 54
+    expect(cube_surface_area(1)).to eq 6
+  end
+end
